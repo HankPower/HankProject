48c352ab04eb965d5aa6d9fcd122b7106ebfbe92
Admin <Hankjing66@163.com>
2017-04-08 15:29:27 +0800

    test

--- src/main/java/com/example/testJenkins.java
+++ src/main/java/com/example/testJenkins.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Administrator on 2017-04-08.
  */
+@RestController
 public class testJenkins {
 
 }
